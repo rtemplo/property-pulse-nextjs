02b9c09b8b3aea3ed5f43dc3e49f801aaa3f828d
Templr <rtemplo@gmail.com>
2025-08-13 12:19:49 -0400

    init tailwind config updates

---
+++ tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
