dc10d5b626615f7252b5ce84fa42ca41a4d40c23
Templr <rtemplo@gmail.com>
2025-08-21 00:27:11 -0400

    add meta data for SEO to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,24 @@
 import type { ReactNode } from "react";
+import type { Metadata } from "next";
+
 import "@/assets/styles/globals.css";
 
 interface RootLayoutProps {
   children: ReactNode;
 }
 
+export const metadata: Metadata = {
+  title: "Property Pulse",
+  keywords: ["real estate", "property news", "market trends"],
+  description: "Stay updated with the latest trends and insights in the real estate market."
+};
+
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html>
+      <body>
+        <main>{children}</main>
+      </body>
     </html>
   );
 };
