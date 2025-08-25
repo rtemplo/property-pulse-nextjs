e6882e11e6c22c2b3f694731acdef70773abd720
Templr <rtemplo@gmail.com>
2025-08-25 17:05:28 -0400

    server side retrieval of params

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,25 +1,11 @@
-'use client';
-import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
-
-const PropertyPage = () => {
-  const router = useRouter();
-  const name = useSearchParams().get('name');
-  const pathname = usePathname();
-
-  const goHome = () => {
-    router.replace('/');
-  };
-
-  const id = useParams().id;
-
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+const PropertyPage = async ({ params }: PageProps) => {
+  const { id } = await params;
   return (
     <div>
       <div>Property Page {id}</div>
-      <div>&quot;name&quot; param: {name}</div>
-      <div>Pathname: {pathname}</div>
-      <br />
-      <hr />
-      <button onClick={goHome}>Go Home</button>
     </div>
   );
 };
