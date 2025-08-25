9860127fce76cdf35682761032d7f90c32d84507
Templr <rtemplo@gmail.com>
2025-08-25 16:21:29 -0400

    client side navigation hooks: useRouter, useParams, useSearchParam, and usePathname

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,5 +1,27 @@
+'use client';
+import { useRouter, useParams, useSearchParams, usePathname } from 'next/navigation';
+
 const PropertyPage = () => {
-  return <div>Property Page</div>;
+  const router = useRouter();
+  const name = useSearchParams().get('name');
+  const pathname = usePathname();
+
+  const goHome = () => {
+    router.replace('/');
+  };
+
+  const id = useParams().id;
+
+  return (
+    <div>
+      <div>Property Page {id}</div>
+      <div>&quot;name&quot; param: {name}</div>
+      <div>Pathname: {pathname}</div>
+      <br />
+      <hr />
+      <button onClick={goHome}>Go Home</button>
+    </div>
+  );
 };
 
 export default PropertyPage;
