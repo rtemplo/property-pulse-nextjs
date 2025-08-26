fd7b0975c6fd294e5e2e5ab4d2d1faf76d2861e9
Templr <rtemplo@gmail.com>
2025-08-26 02:20:20 -0400

    added Navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,24 @@
-import type { ReactNode } from "react";
-import type { Metadata } from "next";
+import type { ReactNode } from 'react';
+import type { Metadata } from 'next';
+import Navbar from '@/components/Navbar';
 
-import "@/assets/styles/globals.css";
+import '@/assets/styles/globals.css';
 
 interface RootLayoutProps {
   children: ReactNode;
 }
 
 export const metadata: Metadata = {
-  title: "Property Pulse",
-  keywords: ["real estate", "property news", "market trends"],
-  description: "Stay updated with the latest trends and insights in the real estate market."
+  title: 'Property Pulse',
+  keywords: ['real estate', 'property news', 'market trends'],
+  description: 'Stay updated with the latest trends and insights in the real estate market.'
 };
 
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html>
       <body>
+        <Navbar />
         <main>{children}</main>
       </body>
     </html>
