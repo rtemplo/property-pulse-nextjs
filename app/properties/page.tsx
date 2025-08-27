cff1d3466f10338ed05e4c130dbf164af77a6d4a
Templr <rtemplo@gmail.com>
2025-08-27 11:47:01 -0400

    create PropertyCard comp for Properties page

--- app/properties/page.tsx
+++ app/properties/page.tsx
@@ -1,3 +1,21 @@
+import PropertyCard from '@/components/PropertyCard';
+import { Property } from '@/types';
+import properties from '@/properties.json';
+
 export default function PropertiesPage() {
-  return <div>Properties Page</div>;
+  return (
+    <section className="px-4 py-8">
+      <div className="container-xl lg:container m-auto px-4 py-4">
+        {properties.length === 0 ? (
+          'No properties found'
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {properties.map((property: Property) => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      </div>
+    </section>
+  );
 }
