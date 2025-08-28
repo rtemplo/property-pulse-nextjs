8d82fceb7711a5398ac968c5c904382b4baf4d31
Templr <rtemplo@gmail.com>
2025-08-28 12:49:04 -0400

    individual property page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,12 +1,40 @@
+import Link from 'next/link';
+import { FaArrowLeft } from 'react-icons/fa';
+import PropertyHeaderImage from '@/components/PropertyHeaderImage';
+import connectDB from '@/config/database';
+import Property from '@/models/Property';
+import { parseSinglePropertyData } from '@/utils/typeGuards';
+import PropertyDetails from '@/components/PropertyDetails';
+
 type PageProps = {
   params: Promise<{ id: string }>;
 };
+
 const PropertyPage = async ({ params }: PageProps) => {
   const { id } = await params;
+  await connectDB();
+  const propertyData: unknown = await Property.findById(id).lean();
+  const property = parseSinglePropertyData(propertyData);
+
   return (
-    <div>
-      <div>Property Page {id}</div>
-    </div>
+    <>
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto py-6 px-6">
+          <Link href="/properties" className="text-blue-500 hover:text-blue-600 flex items-center">
+            <FaArrowLeft className="mr-2" />
+            Back to Properties
+          </Link>
+        </div>
+      </section>
+      <section className="bg-blue-50">
+        <div className="container m-auto py-10 px-6">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            <PropertyDetails property={property} />
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
