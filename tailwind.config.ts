e13d92d8ace0c373a4da2a302df060b8eaa97d0d
Templr <rtemplo@gmail.com>
2025-08-28 17:57:29 -0400

    restore original poppins font config in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,9 +3,9 @@ import type { Config } from 'tailwindcss';
 const config: Config = {
   theme: {
     extend: {
-      // fontFamily: {
-      //   sans: ['Poppins', 'sans-serif']
-      // },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif']
+      },
       gridTemplateColumns: {
         '70/30': '70% 28%'
       }
