b34a2ca7860902fc14fb35c543e76bfcbc26aeac
Templr <rtemplo@gmail.com>
2025-08-28 13:35:51 -0400

    minor delimeter formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,17 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   theme: {
     extend: {
-      fontFamily: {
-        sans: ["Poppins", "sans-serif"],
-      },
+      // fontFamily: {
+      //   sans: ['Poppins', 'sans-serif']
+      // },
       gridTemplateColumns: {
-        "70/30": "70% 28%",
-      },
-    },
+        '70/30': '70% 28%'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 
 export default config;
