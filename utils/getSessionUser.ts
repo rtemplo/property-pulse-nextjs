62e904b58b4bb36c744f66cc45ca2bdb000a060b
Templr <rtemplo@gmail.com>
2025-09-09 11:54:58 -0400

    remove try/catch block as recommended to workaround Vercel deployment issue.

--- utils/getSessionUser.ts
+++ utils/getSessionUser.ts
@@ -3,20 +3,20 @@ import { authOptions } from './authOptions';
 import { isIPropertyPulseSession } from './typeGuards';
 
 export const getSessionUser = async () => {
-  try {
-    const session = await getServerSession(authOptions);
+  // try {
+  const session = await getServerSession(authOptions);
 
-    // Validate that the session matches our expected type
-    if (isIPropertyPulseSession(session) && session?.user?.id) {
-      return {
-        user: session?.user,
-        userId: session?.user?.id
-      };
-    } else {
-      return null;
-    }
-  } catch (error) {
-    console.error('Error fetching session user:', error);
+  // Validate that the session matches our expected type
+  if (isIPropertyPulseSession(session) && session?.user?.id) {
+    return {
+      user: session?.user,
+      userId: session?.user?.id
+    };
+  } else {
     return null;
   }
+  // } catch (error) {
+  //   console.error('Error fetching session user:', error);
+  //   return null;
+  // }
 };
