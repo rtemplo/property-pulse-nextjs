72953202e468ad5edbddf79b3d0aeae39478175e
Templr <rtemplo@gmail.com>
2025-09-09 10:51:06 -0400

    Type and extract property data

--- app/actions/addProperty.ts
+++ app/actions/addProperty.ts
@@ -1,7 +1,36 @@
 'use server';
 
-async function addProperty() {
-  console.log('Add Property Action');
+async function addProperty(formData: FormData) {
+  const propertyData = {
+    type: formData.get('type') as string,
+    name: formData.get('name') as string,
+    description: formData.get('description') as string,
+    location: {
+      street: formData.get('location.street') as string,
+      city: formData.get('location.city') as string,
+      state: formData.get('location.state') as string,
+      zipcode: formData.get('location.zipcode') as string
+    },
+    beds: Number(formData.get('beds')),
+    baths: Number(formData.get('baths')),
+    square_feet: Number(formData.get('square_feet')),
+    amenities: formData.getAll('amenities') as string[],
+    rates: {
+      weekly: formData.get('rates.weekly') ? Number(formData.get('rates.weekly')) : undefined,
+      monthly: formData.get('rates.monthly') ? Number(formData.get('rates.monthly')) : undefined,
+      nightly: formData.get('rates.nightly') ? Number(formData.get('rates.nightly')) : undefined
+    },
+    seller_info: {
+      name: formData.get('seller_info.name') as string,
+      email: formData.get('seller_info.email') as string,
+      phone: formData.get('seller_info.phone') as string
+    },
+    images: formData
+      .getAll('images')
+      .filter((file): file is File => file instanceof File && file.name !== '') as File[]
+  };
+
+  console.log('Property added:', propertyData);
 }
 
 export default addProperty;
