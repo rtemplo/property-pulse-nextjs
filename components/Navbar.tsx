ccdd8258ff44545144f3fc7c5379cb6b31cb4265
Templr <rtemplo@gmail.com>
2025-09-10 19:27:52 -0400

    close drop down menu after selection

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -219,6 +219,7 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex={-1}
                       id="user-menu-item-0"
+                      onClick={() => setIsProfileMenuOpen(false)}
                     >
                       Your Profile
                     </Link>
@@ -228,6 +229,7 @@ const Navbar = () => {
                       role="menuitem"
                       tabIndex={-1}
                       id="user-menu-item-2"
+                      onClick={() => setIsProfileMenuOpen(false)}
                     >
                       Saved Properties
                     </Link>
