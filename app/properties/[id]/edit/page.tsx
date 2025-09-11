b669dfbfe63895b50a3549502baedb22e8de09ec
Templr <rtemplo@gmail.com>
2025-09-10 22:43:02 -0400

    implement ProfileEditForm

---
+++ app/properties/[id]/edit/page.tsx
@@ -0,0 +1,33 @@
+import PropertyEditForm from '@/components/PropertyEditForm';
+import connectDB from '@/config/database';
+import Property, { PropertyDocument, SerializableProperty } from '@/models/Property';
+import { convertToSerializeableObject } from '@/utils/convertToObject';
+
+type PageProps = {
+  params: Promise<{ id: string }>;
+};
+
+const PropertyEditPage: React.FC<PageProps> = async ({ params }) => {
+  const { id } = await params;
+  await connectDB();
+
+  const propertyDoc = (await Property.findById(id).lean()) as PropertyDocument;
+
+  if (!propertyDoc) {
+    return <h1 className="text-center text-2xl font-bold mt-10">Property not found</h1>;
+  }
+
+  const property = convertToSerializeableObject(propertyDoc);
+
+  return (
+    <div className="bg-blue-500">
+      <div className="container m-auto max-w-2xl py-24">
+        <div className="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0">
+          <PropertyEditForm property={property} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyEditPage;
